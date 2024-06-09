e9933d653f666cebdc941c61f99a15a7da81d972
Adesola Adegoke <nestywrites2.0@gmail.com>
2024-06-09 14:45:03 +0100

    Create temp.js

---
+++ temp.js
@@ -0,0 +1,8 @@
+let temp = alert("Let's convert Temperature units together")
+console.log(temp)
+let c2f = alert("Let's convert from Celsius to Fahrenheit")
+console.log(c2f)
+
+let cf = prompt("Enter the fahrenheit:",34);
+let C = (cf - 32 )/ 1.8;
+alert(`The new temperature is: °${C.toFixed(2)}`);
