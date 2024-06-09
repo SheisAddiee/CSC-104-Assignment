c3b578cda8d8ed374f1d902394481a5d66502199
Adesola Adegoke <nestywrites2.0@gmail.com>
2024-06-09 14:43:53 +0100

    Create currency.js

---
+++ currency.js
@@ -0,0 +1,6 @@
+let curr = alert("Let's covert from Dollars to Naira")
+console.log(curr)
+
+let doll = prompt("Enter the dollar amount:", 1200);
+let d2n = doll * 780;
+alert(`The naira equivalent: ₦${d2n.toFixed(2)}`);
